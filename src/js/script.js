77d508c11fbcf39663bb3f28f53a383cbf02223e
nellys1 <gcruz26@aoiths.org>
2026-02-10 13:58:03 -0500

    v1.0

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
 document.querySelector(".btn-primary").addEventListener("click", function (event) {
 
-    const firstName = document.querySelector("");
-
+    const firstName = document.querySelector("#first-name").value;
+    const lastName = document.querySelector("#last-name").value;
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    
+    const country = document.querySelector("#country").value;
+    const acctype = document.querySelector('input[name="accountType"]:checked').value;
+    const abyou = document.querySelector("#about-you").value;
+    
 });
+
